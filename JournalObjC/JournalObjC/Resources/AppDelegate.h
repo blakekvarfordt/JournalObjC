bc2beacc6eb3abdf315b6833b91787a55284e1d1
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-19 12:27:02 -0600

    initial commit with folders

---
+++ JournalObjC/JournalObjC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalObjC
+//
+//  Created by Blake kvarfordt on 8/19/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
