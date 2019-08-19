8a434d38045560a32684fec651d223a54cf3f44f
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-19 15:28:43 -0600

    sets up EntryTableView data source methods

---
+++ JournalObjC/JournalObjC/Views/EntryTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewController.h
+//  JournalObjC
+//
+//  Created by Blake kvarfordt on 8/19/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EntryTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JournalObjC/JournalObjC/Views/EntryTableViewController.m
@@ -0,0 +1,50 @@
+//
+//  EntryTableViewController.m
+//  JournalObjC
+//
+//  Created by Blake kvarfordt on 8/19/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+#import "EntryTableViewController.h"
+#import "EntryController.h"
+#import "Entry.h"
+
+@interface EntryTableViewController ()
+
+@end
+
+@implementation EntryTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return EntryController.shared.entries.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EntryListCell" forIndexPath:indexPath];
+    
+    Entry *entry = EntryController.shared.entries[indexPath.row];
+    
+    cell.textLabel.text = entry.name;
+    
+    return cell;
+}
+
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+  
+}
+
+
+@end
